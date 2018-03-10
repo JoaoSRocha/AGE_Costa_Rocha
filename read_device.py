3cb0714cdae8d453ab82d095af8874bb071ba231
joao175costa <joao175costa@gmail.com>
2018-03-10 21:30:07 +0000

    sampleSubmission added
    Kaggle will score our results according to a provided .csv formatted as
    sampleSubmission

--- read_device.py
+++ read_device.py
@@ -1,1 +1,12 @@
- 
+import pickle
+with open('test_pickle/100006.pkl', 'rb') as f:
+    data = pickle.load(f)    
+    
+import matplotlib.pyplot as plt
+
+from scipy.fftpack import fft
+import numpy as np
+plt.plot(data[:,1])
+ft = fft(data[:,1])
+plt.figure()
+plt.plot(np.imag(ft))
